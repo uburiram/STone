9efcae80fff6c7aed542eabf1d8e9cc2a93fdfa0
uburiram <153625870+uburiram@users.noreply.github.com>
2026-08-04 23:49:22 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* STone Service Worker - network-first for app files (auth-safe) */
-const CACHE_NAME = 'somtum-v15-scope';
+const CACHE_NAME = 'somtum-v16-bugfix';
 const CORE_ASSETS = [
   './',
   './index.html',
