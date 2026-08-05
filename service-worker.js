9e3656911887bdd1214440b6d8cb1a7442cdf05e
uburiram <153625870+uburiram@users.noreply.github.com>
2026-08-05 12:16:48 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* STone Service Worker - network-first for app files (auth-safe) */
-const CACHE_NAME = 'stone-v20260805033321';
+const CACHE_NAME = 'stone-v20260805045728';
 const CORE_ASSETS = [
   './',
   './index.html',
