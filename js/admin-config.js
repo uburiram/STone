80094bb4083f55c68a6f8526d0cd2989840b3603
uburiram <153625870+uburiram@users.noreply.github.com>
2026-08-07 19:39:37 +0700

    Add files via upload

---
+++ js/admin-config.js
@@ -0,0 +1,11 @@
+/**
+ * STone Admin config
+ * - เข้าหน้าแอดมินได้เฉพาะบัญชี Google: uburiram@gmail.com
+ * - หลังล็อกอินต้องใส่รหัสผ่านแอดมิน (เก็บเป็น SHA-256)
+ */
+window.STONE_ADMIN_CONFIG = {
+  emails: ['uburiram@gmail.com'],
+  uids: [],
+  passwordHashSha256:
+    '655918c313cf3d0f1d8794e78045329ba204cce053be2558cea36393c289f7b0'
+};
